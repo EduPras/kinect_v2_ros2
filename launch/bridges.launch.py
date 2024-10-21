252e6cca7f9c6efee7e021d082df6703dcd823c1
Eduardo Prasniewski <eduardoprasniewski@gmail.com>
2024-10-21 20:08:50 -0300

    Update Rviz and bridge Depth Image

--- launch/bridges.launch.py
+++ launch/bridges.launch.py
@@ -9,12 +9,12 @@ def generate_launch_description():
         name='gz_bridge',
         arguments=[
             '/world/pigs/model/kinect_v2_standalong/link/root/sensor/kinect_v2_rgb_sensor/image@sensor_msgs/msg/Image@ignition.msgs.Image',
-            '/world/pigs/model/kinect_v2_standalong/link/root/sensor/kinect_v2_ir_sensor/depth_image/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked'
+            '/world/pigs/model/kinect_v2_standalong/link/root/sensor/kinect_v2_ir_sensor/depth_image@sensor_msgs/msg/Image[ignition.msgs.Image',
             # '/world/pigs/model/kinect_v2_standalong/link/root/sensor/kinect_v2_ir_sensor/depth_image/points\
             #     @sensor_msgs/msg/PointCloud@ignition.msgs.PointCloudPacked'
         ],
         remappings=[
-            ('/world/pigs/model/kinect_v2_standalong/link/root/sensor/kinect_v2_ir_sensor/depth_image/points', 
+            ('/world/pigs/model/kinect_v2_standalong/link/root/sensor/kinect_v2_ir_sensor/depth_image', 
              '/kinect_v2_ir_sensor/depth_image'),
             ('/world/pigs/model/kinect_v2_standalong/link/root/sensor/kinect_v2_rgb_sensor/image', 
              '/kinect_v2_rgb_sensor/image'),
